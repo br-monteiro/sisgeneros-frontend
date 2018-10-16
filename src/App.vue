cd8241ba17bcb7da6f376e7201561c8f03f628ec
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-15 23:26:51 -0300

    Adjustments for template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="container body">
+    <div class="main_container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
+import Vue from 'vue';
 import 'jquery';
+import 'bootstrap/dist/js/bootstrap.min';
 import 'gentelella/vendors/fastclick/lib/fastclick';
 import 'gentelella/vendors/nprogress/nprogress';
+import './build-gentelella/js/custom';
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 
 require('bootstrap/dist/css/bootstrap.min.css');
-// require('gentelella/vendors/font-awesome/css/font-awesome.min.css');
+require('gentelella/vendors/font-awesome/css/font-awesome.min.css');
 require('gentelella/vendors/nprogress/nprogress.css');
 require('./build-gentelella/css/custom.css');
 
