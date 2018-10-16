74228a8a362bc4bdc634c3cb63a491d470d0f860
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-15 23:28:01 -0300

    Adding FooterNavigation component

---
+++ src/components/FooterNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- footer content -->
+    <footer>
+        <div class="pull-right">
+          Sistema de Gerenciamento de Gêneros e Licitações
+        </div>
+        <div class="clearfix"></div>
+    </footer>
+    <!-- /footer content -->
+</template>
+
+<script>
+export default {
+  name: 'footer-navigation',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
