49a306b24f40680527dc625f2d2b504c2d9c9ae5
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-15 23:30:53 -0300

    Adding TemplateDefault component

---
+++ src/components/TemplateDefault.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <menu-right></menu-right>
+    <top-navigation></top-navigation>
+    <slot></slot>
+    <footer-navigation></footer-navigation>
+  </div>
+</template>
+
+<script>
+import MenuRight from './MenuRight';
+import TopNavigation from './TopNavigation';
+import FooterNavigation from './FooterNavigation';
+
+export default {
+  name: 'template-default',
+  components: {
+    MenuRight,
+    TopNavigation,
+    FooterNavigation,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
