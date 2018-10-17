3b0bc1a1b2d432bb6b8ba64cb7f2e0a64d2ab22b
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-16 21:39:43 -0300

    Adding component FormLogin

---
+++ src/components/auth/FormLogin.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div v-show="progress">
+      <loading-bar></loading-bar>
+    </div>
+    <div class="login_wrapper">
+      <div class="animate form login_form">
+        <section class="login_content">
+          <form>
+            <h1>Área de Login</h1>
+            <div>
+              <input type="text" class="form-control" v-model="username" placeholder="Usuário" required="" />
+            </div>
+            <div>
+              <input type="password" class="form-control" v-model="password" placeholder="Senha" required="" />
+            </div>
+            <div>
+              <a class="btn btn-success btn-lg" @click="login">
+                <i class="fa fa-check-circle"></i> Entrar
+              </a>
+            </div>
+            <div class="clearfix"></div>
+            <div class="separator">
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Configurations from '../../common/Configurations';
+import Authenticator from '../../common/Authenticator';
+import LoadingBar from '../LoadingBar';
+
+const baseUrl = Configurations.BASE_URL_PAI;
+
+export default {
+  name: 'form-login',
+  components: {
+    LoadingBar,
+  },
+  data() {
+    return {
+      progress: false,
+      username: '',
+      password: '',
+    };
+  },
+  created() {
+    if (Authenticator.isLoggedIn()) {
+      this.$router.push('/');
+    }
+  },
+  methods: {
+    login() {
+      if (!this.password && !this.username) {
+        this.dialog('O campo Usuário e Senha deve ser preenchido.');
+        return;
+      }
+      // active progress bar
+      this.progress = true;
+
+      Vue.axios.post(baseUrl.concat('auth'), {
+        username: this.username,
+        password: this.password,
+      })
+        .then((response) => {
+          // register JWT for user
+          Authenticator.registerUser(response);
+          // deactivating progress bar
+          this.progress = false;
+          // redirect to home
+          this.$router.push('/');
+        })
+        .catch((response) => {
+          // deactivating progress bar
+          this.progress = false;
+          // dialog window
+          if (response.response && response.response.status === 401) {
+            this.dialog('Não foi possível autenticar o usuário. Verifique se digitou corretamente');
+          } else {
+            this.dialog('Houve um erro ao conectar com servidor...');
+            console.error(response);
+          }
+        });
+    },
+    dialog(message) {
+      const options = { title: 'Atenção', size: 'sm' };
+      this.$dialogs.alert(message, options);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
