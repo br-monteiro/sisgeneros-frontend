08f4cf02136aded0c1291513087b5745f0cd5fff
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-17 12:06:56 -0300

    Register home's routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import Router from 'vue-router';
 import RouterFunctions from '../common/Router';
 // routes
 import auth from './auth';
+import home from './home';
 
 Vue.use(Router);
 
 
 let routes = RouterFunctions.routerConcat([], auth);
-routes = RouterFunctions.routerConcat(routes, []);
+routes = RouterFunctions.routerConcat(routes, home);
 
 export default new Router({
   routes,
