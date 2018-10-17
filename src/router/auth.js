19ac709643742678412a624e14f87b43f767f7cc
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-17 12:10:06 -0300

    Remove unnecessary route

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,16 +1,10 @@
 import FormLogin from '@/components/auth/FormLogin';
-import HelloWorld from '@/components/HelloWorld';
 
 const routes = [
   {
     path: '/login',
     name: 'formLogin',
     component: FormLogin,
-  },
-  {
-    path: '/hello',
-    name: 'HelloWorld',
-    component: HelloWorld,
   },
 ];
 
