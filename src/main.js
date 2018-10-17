90bd41d48191c11c759833ae202b573bd7d7d33c
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-16 21:33:47 -0300

    Adding new libs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import SlimDialog from 'v-slim-dialog';
 import 'jquery';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'gentelella/vendors/fastclick/lib/fastclick';
 import 'gentelella/vendors/nprogress/nprogress';
+import 'v-slim-dialog/dist/v-slim-dialog.css';
 import './build-gentelella/js/custom';
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(SlimDialog);
+Vue.use(VueAxios, axios);
 
 require('bootstrap/dist/css/bootstrap.min.css');
 require('gentelella/vendors/font-awesome/css/font-awesome.min.css');
