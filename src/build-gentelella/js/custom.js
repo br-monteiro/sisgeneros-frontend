fdf99d9da94f5f04d2a80431d05b62dfa3ef2544
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-17 21:25:41 -0300

    Small adjustments

--- src/build-gentelella/js/custom.js
+++ src/build-gentelella/js/custom.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-setTimeout(function() {
+function setup() {
 /**
  * Resize function without multiple trigger
  *
@@ -8,110 +8,103 @@ setTimeout(function() {
  *     // code here
  * });
  */
-(function($,sr){
-    // debouncing function from John Hann
-    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-    var debounce = function (func, threshold, execAsap) {
-      var timeout;
+function r($,sr){
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+    var timeout;
 
-        return function debounced () {
-            var obj = this, args = arguments;
-            function delayed () {
-                if (!execAsap)
-                    func.apply(obj, args);
-                timeout = null;
-            }
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          }
 
-            if (timeout)
-                clearTimeout(timeout);
-            else if (execAsap)
-                func.apply(obj, args);
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
 
-            timeout = setTimeout(delayed, threshold || 100);
-        };
-    };
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  };
 
-    // smartresize
-    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+  // smartresize
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
-})(jQuery,'smartresize');
+};
+
+r(jQuery,'smartresize');
 /**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
-    $BODY = $('body'),
-    $MENU_TOGGLE = $('#menu_toggle'),
-    $SIDEBAR_MENU = $('#sidebar-menu'),
-    $SIDEBAR_FOOTER = $('.sidebar-footer'),
-    $LEFT_COL = $('.left_col'),
-    $RIGHT_COL = $('.right_col'),
-    $NAV_MENU = $('.nav_menu'),
-    $FOOTER = $('footer');
-
+var CURRENT_URL = window.location.href.split('#')[0].split('?')[0];
 // Sidebar
 function init_sidebar() {
 // TODO: This is some kind of easy fix, maybe we can improve this
 var setContentHeight = function () {
     // reset height
-    $RIGHT_COL.css('min-height', $(window).height());
+    $('.right_col').css('min-height', $(window).height());
 
-    var bodyHeight = $BODY.outerHeight(),
-        footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
-        leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+    var bodyHeight = $('body').outerHeight(),
+        footerHeight = $('body').hasClass('footer_fixed') ? -10 : $('footer').height(),
+        leftColHeight = $('.left_col').eq(1).height() + $('.sidebar-footer').height(),
         contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
     // normalize content
-    contentHeight -= $NAV_MENU.height() + footerHeight;
+    contentHeight -= $('.nav_menu').height() + footerHeight;
 
-    $RIGHT_COL.css('min-height', contentHeight);
+    $('.right_col').css('min-height', contentHeight);
 };
 
-  $SIDEBAR_MENU.find('a').on('click', function(ev) {
-      console.log('clicked - sidebar_menu');
-        var $li = $(this).parent();
+$('#sidebar-menu').find('a').on('click', function(ev) {
+  console.log('clicked - sidebar_menu');
+    var $li = $(this).parent();
 
-        if ($li.is('.active')) {
-            $li.removeClass('active active-sm');
-            $('ul:first', $li).slideUp(function() {
-                setContentHeight();
-            });
-        } else {
-            // prevent closing menu if we are on child menu
-            if (!$li.parent().is('.child_menu')) {
-                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                $SIDEBAR_MENU.find('li ul').slideUp();
-            }else
+    if ($li.is('.active')) {
+        $li.removeClass('active active-sm');
+        $('ul:first', $li).slideUp(function() {
+            setContentHeight();
+        });
+    } else {
+        // prevent closing menu if we are on child menu
+        if (!$li.parent().is('.child_menu')) {
+            $('#sidebar-menu').find('li').removeClass('active active-sm');
+            $('#sidebar-menu').find('li ul').slideUp();
+        }else
+        {
+            if ( $('body').is( ".nav-sm" ) )
             {
-                if ( $BODY.is( ".nav-sm" ) )
-                {
-                    $li.parent().find( "li" ).removeClass( "active active-sm" );
-                    $li.parent().find( "li ul" ).slideUp();
-                }
+                $li.parent().find( "li" ).removeClass( "active active-sm" );
+                $li.parent().find( "li ul" ).slideUp();
             }
-            $li.addClass('active');
-
-            $('ul:first', $li).slideDown(function() {
-                setContentHeight();
-            });
         }
-    });
+        $li.addClass('active');
+
+        $('ul:first', $li).slideDown(function() {
+            setContentHeight();
+        });
+    }
+});
 
 // toggle small or large menu
-$MENU_TOGGLE.on('click', function() {
+$('#menu_toggle').on('click', function() {
         console.log('clicked - menu toggle');
 
-        if ($BODY.hasClass('nav-md')) {
-            $SIDEBAR_MENU.find('li.active ul').hide();
-            $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+        if ($('body').hasClass('nav-md')) {
+            $('#sidebar-menu').find('li.active ul').hide();
+            $('#sidebar-menu').find('li.active').addClass('active-sm').removeClass('active');
         } else {
-            $SIDEBAR_MENU.find('li.active-sm ul').show();
-            $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+            $('#sidebar-menu').find('li.active-sm ul').show();
+            $('#sidebar-menu').find('li.active-sm').addClass('active').removeClass('active-sm');
         }
 
-    $BODY.toggleClass('nav-md nav-sm');
+    $('body').toggleClass('nav-md nav-sm');
 
     setContentHeight();
 
@@ -119,9 +112,9 @@ $MENU_TOGGLE.on('click', function() {
 });
 
     // check active menu
-    $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+    $('#sidebar-menu').find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-    $SIDEBAR_MENU.find('a').filter(function () {
+    $('#sidebar-menu').find('a').filter(function () {
         return this.href == CURRENT_URL;
     }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
         setContentHeight();
@@ -2705,7 +2698,7 @@ if (typeof NProgress != 'undefined') {
                   resize: true
                 });
 
-                $MENU_TOGGLE.on('click', function() {
+                $('#menu_toggle').on('click', function() {
                   $(window).resize();
                 });
 
@@ -5030,4 +5023,10 @@ if (typeof NProgress != 'undefined') {
         init_autosize();
         init_autocomplete();
     });
-}, 100);
+}
+setup();
+
+window.addEventListener("hashchange", function() {
+  console.log('run resize');
+  setup();
+});
