3c42335083ccb28d58034b53a297704271dbc824
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-10-22 19:59:34 -0200

    Adding conditions from duplicity message

--- src/common/DialogError.js
+++ src/common/DialogError.js
@@ -6,17 +6,21 @@ const dialog = (component, rawResponse, schemaMessage) => {
     const data = response.data;
     const httpCode = response.status;
     const message = data.message;
-    // const status = data.status;
+    const status = data.status;
     // const details = data.details;
     const error = data.error;
+    let msg;
+
+    // usuário com token expirado
     if (httpCode === 401 && message === 'User with expired authentication') {
       Authenticatior.removeUser();
       component.$router.push('/login');
     }
 
+    // mensagens para campos inválidos
     if (httpCode === 400 && typeof component.dialog === 'function' && message === 'There are wrong fields in submission') {
       if (schemaMessage.maps && error) {
-        let msg = error.length > 1 ? 'Os seguintes campos são obrigatórios: ' : 'O seguinte campo é obrigatório: ';
+        msg = error.length > 1 ? 'Os seguintes campos são obrigatórios: ' : 'O seguinte campo é obrigatório: ';
         msg += error.map((el) => {
           const i = schemaMessage.maps[el.property];
           if (el.constraint === 'required') {
@@ -34,6 +38,15 @@ const dialog = (component, rawResponse, schemaMessage) => {
           return '';
         }).join(', ');
         component.dialog(msg);
+      }
+    }
+
+    // mensagens para duplicidade de registro
+    if (httpCode === 400 && typeof component.dialog === 'function' && status === 'warning') {
+      const matches = /entry\s'(.*?)'.*?'(\w+)_/.exec(message);
+      if (matches.length >= 3) {
+        const i = schemaMessage.maps[matches[2]];
+        component.dialog(`Já existe registro com ${i}: ${matches[1]}`);
       }
     }
   }
