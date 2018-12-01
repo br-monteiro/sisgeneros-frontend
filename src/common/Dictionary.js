faa2ac01098a027be0f016185916c1c98df15f0a
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-12-01 02:41:43 -0200

    Adding words

--- src/common/Dictionary.js
+++ src/common/Dictionary.js
@@ -7,4 +7,24 @@ export default [
     from: 'no',
     to: 'Não',
   },
+  {
+    from: 'root',
+    to: 'Super Usuário',
+  },
+  {
+    from: 'admin',
+    to: 'Administrador',
+  },
+  {
+    from: 'manager',
+    to: 'Gerente',
+  },
+  {
+    from: 'viewer',
+    to: 'Visualizador',
+  },
+  {
+    from: 'requester',
+    to: 'Solicitante',
+  },
 ];
