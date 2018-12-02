481f8c3cff491be7936fafdeb551bb02e3bfa014
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-12-02 17:47:53 -0200

    Refactoring configurations of HOST

--- src/common/Configurations.js
+++ src/common/Configurations.js
@@ -2,6 +2,11 @@ const devHosts = [
   'localhost:8080',
   'dev.api.ceimbe.mb',
 ];
-const BASE_URL_PAI = devHosts.includes(window.location.host) ? '//dev.api.ceimbe.mb/api/v1/' : '//ceimbe.mb/sisgeneros/api/v1/';
+
+const DEV_HOST = '//dev.api.ceimbe.mb/api/v1/';
+
+const PRD_HOST = '//www.ceimbe.mb/app/sisgeneros/api/v1/';
+
+const BASE_URL_PAI = devHosts.includes(window.location.host) ? DEV_HOST : PRD_HOST;
 
 export default { BASE_URL_PAI };
